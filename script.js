67be65a9e6d1c1dff66d4bd2136d861f67df803d
Juan <juan.alquinta@hotmail.com>
2025-05-05 17:56:33 -0400

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const students=[]
+const tableBody=document.querySelector("#studentTable tbody")
+document.getElementById("studentForm").addEventListener("submit",function(e){
+    e.preventDefault();
+
+    const name=document.getElementById("name").value.trim();
+    const lastName=document.getElementById("lastName").value.trim();
+    const grade=parseFloat(document.getElementById("grade").value);
+    
+    if(!name || !lastName || isNaN(grade) || grade<1 || grade>7){
+        return
+    }
+   
+    const student={name,lastName,grade};
+
+    students.push(student);
+    //console.log(students)
+    addStudentTotable(student)
+    this.reset()
+    
+})
+
+function addStudentTotable(student){
+    const row=document.createElement("tr");
+    row.innerHTML=`
+    <td${student.name}</td
+    <td${student.lastName}</td
+    <td${student.grade}</td>`;
+  tableBody.appendChild(row);  
+}
